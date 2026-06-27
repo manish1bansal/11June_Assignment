605de4c00f357d547ec9d807358bf44add382eb3
Manish Bansal <manish_01bansal@yahoo.com>
2026-06-27 14:01:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name="manish"
   location="eastus"
 }
-
-resource "azurerm_storage_account" "stg" {
-  name = "manish-storage"
-  location = "eastus"
-  resource_group_name = "manish"
-  account_tier = "Standard"
-  account_replication_type = "LRS"
-}
